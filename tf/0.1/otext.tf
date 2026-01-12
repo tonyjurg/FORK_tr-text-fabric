ae46c85d5718eec19a0cefde3a4df962f795a472
tonyjurg <github@jurgnet.nl>
2026-01-12 15:15:07 +0100

    testrun after restructure

---
+++ tf/0.1/otext.tf
@@ -0,0 +1,5 @@
+@config
+@version=0.1
+@fmt:text-orig-full={unicode} 
+@sectionTypes=book,chapter,verse
+@sectionFeatures=book,chapter,verse
---
+++ tf/0.1/otype.tf
@@ -0,0 +1,12 @@
+@node
+@description=node type assignment
+@valueType=str
+@version=0.1
+
+1-140726	w
+140727-148683	verse
+148684-148943	chapter
+148944-148970	book
+148971-167820	clause
+167821-235177	phrase
+235178-272531	wg
